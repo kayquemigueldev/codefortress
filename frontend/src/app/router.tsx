32898ff99663a07588de5ac0a46c2a8009f31b89
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-09-13 01:45:25 -0300

    feat: redesign security rules page

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -27,6 +27,7 @@ import '../styles/project-details-v2.css'
 import '../styles/analyses-v2.css'
 import '../styles/findings-v2.css'
 import '../styles/project-form-v2.css'
+import '../styles/security-rules-v2.css'
 import '../styles/auth.css'
 import '../styles/security-rules.css'
 
